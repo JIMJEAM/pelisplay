acc2f55e437e3408e6869215a3d0a4eae07d2403
TVPRO20 <66882914+TVPRO20@users.noreply.github.com>
2025-09-07 07:27:08 -0500

    1233

--- dist/pelis.js
+++ dist/pelis.js
@@ -13,6 +13,47 @@ class Menu extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `<div style="conten-visivility: auto;">
+    
+<ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
+    <li class="cursor-pointer"><a href="https://hexload.com/tllvx16t3lmk"  target="_blank "><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzQMlwlxMXb5b8HtRFveEc6EZr9Smw9TFVGw&s" alt="
+Jurassic World: Renace (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://pixeldrain.com/u/4UfaL5ie"  target="_blank "><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN-uBUVC6frdpV_7j9uPI_fIoQWU2wqRZV5A&s" alt="
+ Pitufos (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://hexload.com/174npjwm3b0e"  target="_blank "><img src="https://m.media-amazon.com/images/M/MV5BNmZjODEzNGMtYjdiNi00ZGEyLWE5MGUtYTUxZmMyOThkNTY0XkEyXkFqcGc@._V1_.jpg" alt="
+ Los Cuatro Fantásticos: Primeros pasos (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    </ul>
+
+
+  
+    <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
+    <li class="cursor-pointer"><a href="https://pixeldrain.com/u/crxtuuCX"  target="_blank "><img src="https://imgs.search.brave.com/sxCslCUrDBNKOGB3KzkvnDR1-bf3uxI3hplIm6O9cc4/rs:fit:860:0:0:0/g:ce/aHR0cDovL3d3dy5p/bXBhd2FyZHMuY29t/LzIwMjUvcG9zdGVy/cy9tZWdhbl90d29f/cG9pbnRfb192ZXIy/LmpwZw" alt="
+M3GAN 2.0 (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://powcloud.one/lh2jjh6e6085"  target="_blank "><img src="https://imgs.search.brave.com/fKXi2kDwSXVZCfA_dUCEV7mspH5aNPGsvDQ3P0rEfNg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZmFuZGFuZ28u/Y29tL0ltYWdlUmVu/ZGVyZXIvODIwLzAv/cmVkZXNpZ24vc3Rh/dGljL2ltZy9kZWZh/dWx0X3Bvc3Rlci5w/bmcvMC9pbWFnZXMv/bWFzdGVycmVwb3Np/dG9yeS9mYW5kYW5n/by8yMzIyMTcvRUxJ/T19QYXlvZmZfUnVu/bmluZ18xc192NDBf/TWVjaDRiX0ZTLmpw/Zw" alt="
+ Elio (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://powcloud.one/b0jdcgvwtwr4"  target="_blank "><img src="https://imgs.search.brave.com/A1q420dan4Z7bjSLo106auQM--EARUzQPrKMVwOB87U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly92ZXJ0/aWdvZmlsbXMuZXMv/d3AtY29udGVudC91/cGxvYWRzLzIwMjQv/MTEvQXVuLUVzdG95/LUFxdWktUG9zdGVy/LTcweDEwMGNtLWdv/eWEtc2NhbGVkLmpw/Zw" alt="
+ Aún Estoy Aquí (2024)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    </ul>
+
+    <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
+    <li class="cursor-pointer"><a href="https://powcloud.one/wtizxn0d50zd"  target="_blank "><img src="https://imgs.search.brave.com/Sdau3-_66R-y3bF4jn5465A41kTjW1T2g_pLkDTJCxk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZXNp/emVyLmdsYW5hY2lv/bi5jb20vcmVzaXpl/ci92Mi9HV1RDSzNE/MlRSQ1JYT05QUE1N/SUlTMzJIVS5wbmc_/YXV0aD00OWQ4MGMw/ZDhhNjEwODQzZTdj/MzlhMDBjZTk3ZWM2/ODQxZDdmNTMzZTM3/Yzk5ZmM3NTVjZDQ5/MjA5YTRkZDM1Jndp/ZHRoPTIxMCZoZWln/aHQ9MzAwJnNtYXJ0/PXRydWU" alt="
+ El amateur: Operación Venganza (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://powcloud.one/1kvyd0t99xdi"  target="_blank "><img src="https://imgs.search.brave.com/k3IWVycpKJLWWF-AdBbWwcPI8IevTmzl45P6jZEIU40/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bXViaXMuZXMvbWVk/aWEvbW92aWVzLzgx/NDUvMzQ1NTM3LzI4/LWFub3MtZGVzcHVl/cy1sX2NvdmVyLmpw/Zw" alt="
+ 28 años después (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://powcloud.one/hazjcxooj7lr"  target="_blank "><img src="https://imgs.search.brave.com/isph-YadexmVGwxSVNfwH2xOz5sU4gxrFK0oPMW9Rls/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1QlkyRTVZakUy/WVRRdFpHSXhZaTAw/WVRVNExUazNZekl0/WW1NeVpUTTNNMkk1/WlRNM1hrRXlYa0Zx/Y0djQC5qcGc" alt="
+ Cómo entrenar a tu dragón (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    </ul>
+
+    
+    <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
+    <li class="cursor-pointer"><a href="https://powcloud.one/ww4pyheq03v2"  target="_blank "><img src="https://image.tmdb.org/t/p/original/5GZeoDCS9kIeTKT7oPejIr0r7LG.jpg" alt="
+ Destino final: Lazos de sangre (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    <li class="cursor-pointer"><a href="https://powcloud.one/xli7m3dg4yh5"  target="_blank "><img src="https://http2.mlstatic.com/D_NQ_NP_952522-MLM82846236583_032025-O-poster-adherible-lilo-stitch-2025-5-40x28cm.webp" alt="
+ Lilo y Stitch (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+      <li class="cursor-pointer"><a href="https://powcloud.one/oa9i5cqk1x2l"  target="_blank "><img src="https://pics.filmaffinity.com/Misiaon_imposible_Sentencia_final-672350282-large.jpg" alt="
+Misión imposible: La sentencia final (2025)" class="img_video_nav img-thumbnail   w-50 "></a></li>
+    </ul>
+    
+
      <ul id="video_navigation" style="list-style-type: none; display: grid; grid-template-columns: repeat(3 , 1fr);">
     <li class="cursor-pointer"> <img src="https://pics.filmaffinity.com/Una_pelaicula_de_Minecraft-276344892-large.jpg" alt="Minecraft (2025)" class="img_video_nav img-thumbnail w-50" onclick="videoUrl('https://ia800807.us.archive.org/5/items/cubos-el-juego-2025/Cubos%20el%20juego%20%282025%29.mp4')"> </li>
     <li class="cursor-pointer"> <img src="https://static.cinepolis.com/resources/mx/movies/posters/414x603/49551-615592-20250314041252.jpg" alt="Rescate implacable (2025)" class="img_video_nav img-thumbnail w-50" onclick="videoUrl('https://ia600708.us.archive.org/28/items/vaca-super-rescue/vaca%20super%20Rescue.mp4')"> </li> 
